ab44d326f5abe4ec808e6bdc96ef27a84b433ef5
zcbe <151560157+zcbe@users.noreply.github.com>
2024-02-20 23:33:44 +0100

    add react router
    Étape 2 : Ajoutez React Router
    Étape 4 : Créez la page d’accueil

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
+const router =createBrowserRouter([
+{
+  path:"/",
+  element: <App />
+},
+{
+    path:"/flat",
+    element:<h1>Nos appartements</h1>
+},
+{
+  path:"/about",
+  element:<h1>A propos</h1>
+},
+{
+  path:"/404",
+  element:<h1>404</h1>
+},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <App/>
+  <RouterProvider router={router} />
   </React.StrictMode>,
 )
