b86ba6b9f8fd45a25394d67737814f2a543573ba
zcbe <151560157+zcbe@users.noreply.github.com>
2024-02-26 00:25:26 +0100

    add props for accueil page

--- src/components/ApartmentCard.jsx
+++ src/components/ApartmentCard.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import "./ApartmentCard.scss"
 import { Link } from "react-router-dom"
  
-function Apartment() {
+function ApartmentCard(props) {
+  console.log("props dans apartment card:", props);
+
   return (
-    <div className='apartment'>
       <Link to="/flat">
-      <div className='apartment__subtitle'>Titre de la location</div>
+        <div className='apartment'>
+          <img src={props.imageUrl} alt="" />
+          <div className='apartment__subtitle'>{props.title}</div>
+        </div>
 
       </Link>
-    </div>
+  
   )
 }
 
 
-export default Apartment;
+export default ApartmentCard;
--- src/components/ApartmentGrid.jsx
+++ src/components/ApartmentGrid.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './ApartmenGrid.scss'
 import ApartmentCard from './ApartmentCard.jsx'
 
+
 function ApartmentGrid() {
+  const [apartments, setApartments] = useState([]);
+  
+  useEffect(() => {
+    fetchApartments (); 
+  }, []);
+  //useEffect avec une array vide == exécute cette fonction au chargement du composant
+
+function fetchApartments() {   
+  fetch("db.json")
+  .then((res) => (res.json()))
+  .then((res) => setApartments(res))
+  .catch(console.error);
+
+}
   return (
-    <div className='grid'>
-        <ApartmentCard/>
-        <ApartmentCard/>
-        <ApartmentCard/>
-        <ApartmentCard/>
-        <ApartmentCard/>
-        <ApartmentCard/>
+    <div className="grid">
+        {apartments.map((apartment) => (
+          <ApartmentCard title = {apartment.title} imageUrl={apartment.cover}/>
+        ))}
+         
+
+      
+       
     </div>
-  )
+  );
 }
 
 export default ApartmentGrid
