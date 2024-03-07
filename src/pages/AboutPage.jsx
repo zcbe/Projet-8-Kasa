a9d6b9c57fd4f64021d0646fec5fba1584d5e595
zcbe <151560157+zcbe@users.noreply.github.com>
2024-03-07 22:57:44 +0100

    fix last modifications for présentation

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -11,10 +11,10 @@ export class AboutPage extends Component {
           <ImageBanner />
         </div>
         <div className="about__container">
-          <Collapse title="Fiabilité" content="..." />
-          <Collapse title="Respect" content="..." />
-          <Collapse title="Service" content="..." />
-          <Collapse title="Sécurité" content="..." />
+          <Collapse title="Fiabilité" content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." />
+          <Collapse title="Respect" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de pertubation du voisinage entraînera une exclusion de notre plateforme." />
+          <Collapse title="Service" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de pertubation du voisinage entraînera une exclusion de notre plateforme." />
+          <Collapse title="Sécurité" content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
         </div>
       </>
     );
--- src/pages/ErrorPageNotFound.jsx
+++ src/pages/ErrorPageNotFound.jsx
@@ -12,7 +12,7 @@ function ErrorPageNotFound() {
       <Main>
         <div className="error-page">
           <h1>404</h1>
-          <h2>Oops! La page que vous demandez n'existe pas</h2>
+          <h2>Oups! La page que vous demandez n'existe pas</h2>
           <Link to="/">Retourner sur la page d'accueil</Link>
         </div>
       </Main>
