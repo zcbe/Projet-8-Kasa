62a01f8446c77b95394395d8fff54abccb4fbefd
zcbe <151560157+zcbe@users.noreply.github.com>
2024-03-10 14:27:46 +0100

    fix last modification for soutenance

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/routes.jsx"; // Importez le routeur depuis son emplacement correct
+import { router } from "./routes/routes.jsx"; 
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} /> {/* Utilisez le routeur importé */}
+    <RouterProvider router={router} /> 
   </React.StrictMode>
 );
